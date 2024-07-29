7c84cbcd3464afbbc507a3ca9b7880068a25a2b5
Tahmid Akter <tahmidakter56@gmail.com>
2024-07-29 23:52:43 +0600

    create view added

--- src/views/to_do/mod.rs
+++ src/views/to_do/mod.rs
@@ -1,9 +1,10 @@
 mod create;
-use actix_web::web::{ServiceConfig, get, scope};
+use actix_web::web::{get, post, scope, ServiceConfig};
 
 pub fn to_do_views_factory(app: &mut ServiceConfig){
     app.service(
         scope("v1/item")
         .route("create/{title}", get().to(create::create))
+        // .route("create/{title}", post().to(create::create))
     );
 }
